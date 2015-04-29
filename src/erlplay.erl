71a2b9c25204c61744036958bc311f627a19dcf3
richard-gibson <rickityg@gmail.com>
2015-04-29 21:55:51 +0100

    setup

---
+++ src/erlplay.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author richard
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29. Apr 2015 21:53
+%%%-------------------------------------------------------------------
+-module(erlplay).
+-author("richard").
+
+%% API
+-export([start/0]).
+
+start() -> io:format("Application started~n").
