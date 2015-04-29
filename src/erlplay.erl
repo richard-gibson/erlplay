70e744714d41133c94a23c864c6edebfed06d0ad
richard-gibson <rickityg@gmail.com>
2015-04-29 22:18:57 +0100

    setup

--- src/erlplay.erl
+++ src/erlplay.erl
@@ -12,4 +12,20 @@
 %% API
 -export([start/0]).
 
-start() -> io:format("Application started~n").
+start() ->
+  Handle = bitcask:open("erlplay_database", [read_write]),
+  ErlplayKey = <<"erlplay_executions">>,
+  N = fetch(Handle,ErlplayKey),
+  store(Handle,ErlplayKey, N+1),
+  io:format("Application has been run ~p times~n",[N]),
+  bitcask:close(Handle),
+  init:stop().
+
+store(Handle,PutKey,N) ->
+  bitcask:put(Handle, PutKey, term_to_binary(N)).
+
+fetch(Handle,GetKey) ->
+  case bitcask:get(Handle,GetKey) of
+    not_found -> 1;
+    {ok, Bin} -> binary_to_term(Bin)
+  end.
